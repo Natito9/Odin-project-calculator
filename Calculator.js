cb15f697a80d2853bd8451d7d7fbda85869f7bbc
Natito9 <179492180+Natito9@users.noreply.github.com>
2024-09-19 11:25:11 +0200

    step 1

---
+++ Calculator.js
@@ -0,0 +1,34 @@
+//pseudo code
+
+//Create basic functions for (ADD;SUBSTRACT;MULTOPLY;DIVIDE),
+//test on console
+//2. Create 3 variables for each of the parts ( 3 + 5)
+//3. create function (oparate) that takes an operator and 2 numbers
+// then calls one of the functions on the numbers
+// 4. add HTML, button for numbers, and opertion and =, clear button
+// 5. connect the HTML with java, (STORE THE DISPLAY VALUE) in a variable
+// 6. make it work. display the result.
+//7. IDK check ODIN
+
+let a = 1;
+let b = 2;
+
+function add (a,b){
+    return a + b;
+}
+function substract (a,b){
+    return a - b;
+}
+function multiply (a,b){
+    return a * b;
+}
+function divide (a,b){
+    return a / b;
+}
+
+
+console.log(add(a,b));
+console.log(substract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
+
